61cf6136bdad3ef2e494ef35ec6d8ac2de6f35f7
iliasagnaou <32297108+iliasagnaou@users.noreply.github.com>
2018-05-14 00:04:59 +0200

    Add files via upload

--- Game/javascript/galgje.js
+++ Game/javascript/galgje.js
@@ -1,1 +1,96 @@
+/*globals $:false,confirm:false,window:false */
+var ITWoorden, GekozenLetter, placeholder = "", TotaalGokken = 0;
+var TotaleGoedeLetters = 0;
+var AantalKansenOver = 12;
+var alfabet, WordPicker, Woord;
+var i, j;
+var UppWoord;
+var arr;
+var placeholderArr;
+var result;
+ITWoorden = ["fotografie", "datacommunicatie", "programmeren", "hardware", "business", "database", "netwerk", "beveiliging", "internet", "software", "besturingssysteem", "processor", "rekenblad", "microsoft", "apple", "cryptocurrencies", "algoritme", "applicatie", "array", "autoincrement", "router", "modem", "monitor", "toetsenbord", "muis", "kabels", "interface", "playstation", "bureaustoel", "programmeur", "printers", "geheugen", "stroom", "scanners", "computerconfiguratie", "protocollen", "behuizing", "domeinnaam", "hexadecimaal", "binair", "emoticons", "internetmarketing", "netwerkkaart"];
+alfabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+function updateOplossing(GekozenLetter) {
+    "use strict";
+    UppWoord = Woord.toUpperCase();
+    arr = UppWoord.split(""); // ["W", "h", "i", "t", "e", " ", "D", "o", "g"]
+    placeholderArr = placeholder.split("");
+    for (i = 0; i < arr.length; i += 1) {
+        if (GekozenLetter === arr[i]) {
+            placeholderArr.splice(i, 1, GekozenLetter);
+        }
+    }
+    result = placeholderArr.join(""); // "White Fog"
+    placeholder = result;
+    $("#LetterHolder").text(placeholder);
+}
+function updateGoedePogingen() {
+    "use strict";
+    TotaleGoedeLetters += 1;
+    $("#GoedeGokken").text(TotaleGoedeLetters);
+}
+function updateImg(AantalKansenOver) {
+    "use strict";
+    $("#galgje_kansen").attr("src", "GalgjeImages/galg" + AantalKansenOver + ".png");
 
+}
+function updateKansenOver() {
+    "use strict";
+    AantalKansenOver -= 1;
+    $("#AantalKansenOver").text(AantalKansenOver);
+    updateImg(AantalKansenOver);
+}
+
+function updatePogingen() {
+    "use strict";
+    TotaalGokken += 1;
+    $("#TotaalGokken").text(TotaalGokken);
+}
+function checkWinst() {
+    "use strict";
+    if (AantalKansenOver === 0) {
+        $('button').prop('disabled', true);
+        if (confirm("Het woord was " + "'" + Woord + "'. " + "Wilt u opnieuw proberen?")) {
+            window.location.reload();
+        }
+    } else if (Woord.toUpperCase() === placeholder) {
+        if (confirm("U won.  Wilt u opnieuw spelen?")) {
+            window.location.reload();
+        }
+    }
+}
+function checkLetter(GekozenLetter) {
+    "use strict";
+    if (Woord.toUpperCase().includes(GekozenLetter)) {
+        updateOplossing(GekozenLetter);
+        updateGoedePogingen();
+    } else {
+        updateKansenOver(GekozenLetter);
+    }
+    updatePogingen();
+    checkWinst();
+}
+$(function () {
+    "use strict";
+    $("button").click(function () {
+        GekozenLetter = $(this).val();
+        checkLetter(GekozenLetter);
+        $(this).prop("disabled", true);
+    });
+});
+function init() {
+    "use strict";
+    WordPicker = Math.floor(Math.random() * ITWoorden.length);
+    Woord = ITWoorden[WordPicker];
+    $("#AantalKansenOver").text(AantalKansenOver);
+    updateImg(AantalKansenOver);
+    for (i = 0; i < alfabet.length; i += 1) {
+        $("#alfabet_div").append("<button value=" + alfabet[i] + ">" + alfabet[i] + "</button>");
+    }
+    for (j = 0; j < Woord.length; j += 1) {
+        placeholder += "_";
+        
+    }
+    $("#LetterHolder").append(placeholder);
+}
+init();
