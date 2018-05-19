8bcd66cc0d1f5ee1eff193a47c64c94523a53681
iliasagnaou <32297108+iliasagnaou@users.noreply.github.com>
2018-05-19 20:52:52 +0200

    Add files via upload

--- Game/javascript/galgje.js
+++ Game/javascript/galgje.js
@@ -1,4 +1,6 @@
 /*globals $:false,confirm:false,window:false */
+
+//declaraties
 var ITWoorden, GekozenLetter, placeholder = "",
     TotaalGokken = 0;
 var TotaleGoedeLetters = 0;
@@ -10,9 +12,13 @@ var arr;
 var placeholderArr;
 var result;
 
+//array met al de woorden voor galgje
 ITWoorden = ["fotografie", "datacommunicatie", "programmeren", "hardware", "business", "database", "netwerk", "beveiliging", "internet", "software", "besturingssysteem", "processor", "rekenblad", "microsoft", "apple", "cryptocurrencies", "algoritme", "applicatie", "array", "autoincrement", "router", "modem", "monitor", "toetsenbord", "muis", "kabels", "interface", "playstation", "bureaustoel", "programmeur", "printers", "geheugen", "stroom", "scanners", "computerconfiguratie", "protocollen", "behuizing", "domeinnaam", "hexadecimaal", "binair", "emoticons", "internetmarketing", "netwerkkaart"];
+
+//het alfabet om letters te kiezen
 alfabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+//Kiest telkens een nieuw woord wanneer er gerefreshed word
 function updateOplossing(GekozenLetter) {
     "use strict";
     UppWoord = Woord.toUpperCase();
@@ -28,18 +34,23 @@ function updateOplossing(GekozenLetter) {
     $("#LetterHolder").text(placeholder);
 }
 
+//bij het juist raden van een letter, word "totalegoedeletters" met 1 opgeteld
 function updateGoedePogingen() {
     "use strict";
     TotaleGoedeLetters += 1;
     $("#GoedeGokken").text(TotaleGoedeLetters);
 }
 
+//bij elke fout dat er gemaakt word, word de afbeelding geupdated.
+//in mapje galgje images kan u zien dat ik aftel van 12 naar 0 met 0 als einde of dood.
+//de tekening was zelfgemaakt.
 function updateImg(AantalKansenOver) {
     "use strict";
     $("#galgje_kansen").attr("src", "GalgjeImages/galg" + AantalKansenOver + ".jpg");
-
 }
 
+//bij elke fout dat er gemaakt word, word er afgeteld van 12 naar 0
+//de functie updateImg word hier opgeroepen.
 function updateKansenOver() {
     "use strict";
     AantalKansenOver -= 1;
@@ -47,12 +58,18 @@ function updateKansenOver() {
     updateImg(AantalKansenOver);
 }
 
+//de totaal aantal keren dat wij gokken, word met 1 opgeteld.
 function updatePogingen() {
     "use strict";
     TotaalGokken += 1;
     $("#TotaalGokken").text(TotaalGokken);
 }
-
+//als aantal kansen 0 is word, worden alle knoppen op 'disabled'
+//de gameSound word gepauzeerd en de loseSound word afgespeeld.
+//dit gaat gepaard met een confirm box waar dat er word meegegeven wat het woord was
+//bij het klikken op oke word het spel opnieuw opgestart
+//en anders als het woord geraden woord word weergegeven, stopt gameSound en start de YouWinSound
+//dit gaat gepaard met een confirm box, bij het klikken op oke word het spel opnieuw opgestart.
 function checkWinst() {
     "use strict";
     if (AantalKansenOver === 0) {
@@ -71,6 +88,11 @@ function checkWinst() {
     }
 }
 
+//Als het gekozen letter in het woord bevind, dan GoedeLetterSound,
+//en worden de functie updateOplossing en updateGoedePogingen opgeroepen
+//zo niet, word functie updatePogingen.
+//en anders word er weergegeven hoeveel pogingen er nog overblijven
+//en functie checkWinst, om te controleren of je gewonnen hebt of verloren.
 function checkLetter(GekozenLetter) {
     "use strict";
     if (Woord.toUpperCase().includes(GekozenLetter)) {
@@ -83,6 +105,8 @@ function checkLetter(GekozenLetter) {
     updatePogingen();
     checkWinst();
 }
+//als er een letter word gekozen, word functie gekozenLetter opgeroepen
+//en de knop zal disabled worden.
 $(function () {
     "use strict";
     $("button").click(function () {
@@ -91,6 +115,16 @@ $(function () {
         $(this).prop("disabled", true);
     });
 });
+
+/*deze functie heet de init functie. Hier word het spel word opgestart
+hier word er random een woord gekozen in de array ITWoorden
+hiervoor heb ik jquery gebruikt zodat ik de nodige html elementen kon oproepen
+updateImg naar mate er aantalKansenOver zijn.
+i stelt al de letters van het alfabet voor
+dus aan de door mij gedeclareerd "var alfabet" word de array i gekoppeld en aangetoond dat het een button is.
+in de tweede for loop word het woord weergegeven in de placeholder als de letters geraden worden
+elk letter dat nog niet geraden is, is een "_". 
+*/
 
 function init() {
     "use strict";
@@ -109,6 +143,7 @@ function init() {
     playGameSound();
 }
 
+/************BEGIN GameAudio***************/
 function playGameSound() {
     "use strict";
     document.getElementById('gameSound').play();
@@ -133,6 +168,6 @@ function youWin() {
     "use strict";
     document.getElementById('youWin').play();
 }
+/************EINDE GameAudio***************/
 
 init();
-//responsive design
--- Game/javascript/loading.js
+++ Game/javascript/loading.js
@@ -1,3 +1,4 @@
+//functie voor de loading screen van het spel
 function progress() {
     "use strict";
     var prg = document.getElementById('progress');
@@ -6,6 +7,11 @@ function progress() {
     var progress = 25;
     var id = setInterval(frame, 50);
     
+    //in deze functie zal is progress, 500px breed, en de counter 100%
+    //id staat op setInterval 50, 50 staat op hoe snel het geladen zal worden
+    //dus als progress 500px breed is en counter op 100% staat word de clearInterval() opgeroepen
+    //en zal index.html -> galgje.html openen "_self" zal de gelinkte document openen in hetzelfde venster
+    //anders word progress met 5px opgeteld en de teller met 1%.
     function frame() {
         if (progress == 500 && counter == 100) {
             clearInterval();
@@ -18,5 +24,5 @@ function progress() {
         }
     }
 }
-
+//hier word de hele functie opgeteld.
 progress();
